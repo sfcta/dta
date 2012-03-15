d97fcf20908a8dc3a62f452ae4f2f211111e7488
Michail Xyntarakis <xyntarakis@pbworld.com>
2012-03-15 15:06:51 -0400

    Added the two modules that read and convert tpplus transit routes to dynameq transit routes. These two modules need to be refactored to work with dta.

---
+++ dta/TPPlusTransitRoute.py
@@ -0,0 +1,237 @@
+__copyright__   = "Copyright 2011 SFCTA"
+__license__     = """
+    This file is part of DTA.
+
+    DTA is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DTA is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with DTA.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import re
+from itertools import izip
+import nose.tools
+from pyparsing import *
+from pbCore.utils.odict import OrderedDict
+from pbCore.utils.paragraph import iterRecords
+
+from pbCore.tpplus.errors import TPPlusError
+
+
+def createTPPlusTransitRouteParser():
+    """Create two parsers on the the header and one for the body containing hte nodes
+    I may be able to create one parser if I can deal with N= wich has the same format with the
+    header expresions. My problem is that I cannot get the parser to see the N= not as a header 
+    expression but as a block of data"""
+
+    lineName = Word(alphanums +" "+"#" + ":" + "_" + "-" + "/" + "\\" + "(" + ")" +"." + "&" + '"') + Literal(',').suppress()
+    expr = Word(alphanums + '[' + ']') + Literal('=').suppress() + Word(alphanums + '"' + '.') + Literal(',').suppress()
+    header = Literal('LINE NAME=') + lineName + OneOrMore(expr) 
+
+
+    nodeId = Word(nums + '-') + ZeroOrMore(Literal(',').suppress())
+    access = Optional('ACCESS=' + Word(nums) + ZeroOrMore(Literal(',').suppress()))
+    segment = Group('N=' + OneOrMore(nodeId) + access)
+    body = OneOrMore(segment)
+
+    route = header + body
+
+    return header.parseString, body.parseString
+
+def parseRoute(net, routeAsString, includeOnlyNetNodes=False):
+
+    header = routeAsString[0:routeAsString.find('N=')]
+    body = routeAsString[routeAsString.find('N='):]
+    
+    headerParser, bodyParser = createTPPlusTransitRouteParser()
+    headerFields = headerParser(header).asList()
+    bodyFields = bodyParser(body)
+
+    if 'LINE NAME=' not in headerFields:
+        raise TPPlusError("I cannot create a route from %s" % str(headerFields))
+    name = headerFields[headerFields.index('LINE NAME=') + 1]
+    
+    route = TPPlusTransitRoute(net, name)
+
+    #TODO I do not like the following
+    #print '\n\n', headerFields, '\n\n'
+    #nose.tools.set_trace()
+
+    for extAttrName, intAttrName in TPPlusTransitRoute.attrs.iteritems():
+        if extAttrName in headerFields:
+            setattr(route, intAttrName, headerFields[headerFields.index(extAttrName) + 1])
+        
+    for segment in bodyFields:
+        access = TransitNode.ACCESS_BOTH
+        if segment[-2] == 'ACCESS=':
+            access == segment[-1]
+            nodeIds = segment[1:-2]
+        else:
+            nodeIds = segment[1:]
+            
+        for nodeId in nodeIds:
+            isStop = True
+            if nodeId.startswith('-'):
+                isStop = False
+                nodeId = nodeId[1:]
+            if includeOnlyNetNodes:
+                if not net.hasNode(nodeId):
+                    continue
+
+            route.addTransitNode(nodeId, isStop, access)
+
+    return route
+        
+class TransitNode(object):
+
+    ACCESS_BOARD = '1'
+    ACCESS_ALIGHT = '2'
+    ACCESS_BOTH = '0'
+
+    def __init__(self, nodeId, isStop, access):
+        
+        self.node = None
+        self.nodeId = nodeId
+        self.isStop = isStop
+        self.access = access 
+
+    def __repr__(self):
+        if self.isStop:
+            return "%s, ACCESS=%s" % (self.nodeId, self.access)
+        else:
+            return "-%s, ACCESS=%s" % (self.nodeId, self.access)
+
+class TPPlusTransitRoute(object):
+
+    extAttributes = ['RUNTIME', 'ONEWAY', 'MODE', 'OWNER', 'XYSPEED', 'TIMEFAC', 'FREQ[1]', 
+                     'FREQ[2]', 'FREQ[3]', 'FREQ[4]', 'FREQ[5]']
+    intAttributes = ['runtime', 'oneway', 'mode', 'owner', 'xySpeed', 'timefac', 'freq1',
+                     'freq2', 'freq3', 'freq4', 'freq5']
+
+    attrs = OrderedDict(izip(extAttributes, intAttributes))
+
+    @classmethod
+    def read(cls, net, fileName, includeOnlyNetNodes=False):
+
+        inputStream = open(fileName, 'r')
+        for record in iterRecords(inputStream, is_separator=re.compile(r'^LINE NAME')):
+
+            transitRoute = parseRoute(net, record, includeOnlyNetNodes=includeOnlyNetNodes)
+
+            yield transitRoute
+        
+    
+    def __init__(self, net, name):
+        
+        self._net = net
+        if name.startswith('"') and name.endswith('"'):
+            name = name[1:-1]
+        self.name = name
+        self.color = None
+        self.mode = None
+        self.oneway = None
+        self.owner = None
+        self.timefac = None
+        self.xySpeed = None
+        self.freq1 = None
+        self.freq2 = None
+        self.freq3 = None
+        self.freq4 = None
+        self.freq5 = None
+        self.runtime = None
+        self._transitNodes = []
+
+        self.attributes = {'RUNTIME':self.__dict__['runtime'], 
+                           'ONEWAY':self.__dict__['oneway'],
+                           'MODE':self.__dict__['mode'],
+                           'OWNER':self.__dict__['owner'],
+                           'XYSPEED':self.__dict__['xySpeed'],
+                           'TIMEFAC':self.__dict__['timefac'],
+                           'FREQ[1]':self.__dict__['freq1'],
+                           'FREQ[2]':self.__dict__['freq2'],
+                           'FREQ[3]':self.__dict__['freq3'],
+                           'FREQ[4]':self.__dict__['freq4'],
+                           'FREQ[5]':self.__dict__['freq5']}
+
+    def __repr__(self):
+        
+        header = 'LINE NAME=%s' % self.name
+        
+        for extAttrName, intAttrName in TPPlusTransitRoute.attrs.iteritems():
+            attrValue = getattr(self, intAttrName)
+            if attrValue:
+                header += ', %s=%s' % (extAttrName, str(attrValue))
+                
+        body = ', N='
+        for transitNode in self.iterTransitNodes():
+            if transitNode.isStop:
+                body += '%s, ' % transitNode.nodeId
+            else:
+                body += '-%s, ' % transitNode.nodeId
+
+        return header + body[:-2] + '\n'
+        
+    def addTransitNode(self, nodeId, isStop, access):
+        """
+        Add a node with the given input id to the transit route
+        """        
+        transitNode = TransitNode(nodeId, isStop, access)
+        self._transitNodes.append(transitNode)
+
+    def getTransitNode(self, nodeId):
+        """Return the transit node with the given id"""
+        for transitNode in self.iterTransitNodes():
+            if transitNode.nodeId == nodeId:
+                return transitNode
+        raise TPPlusError("Node %s is not in the route %s" % (nodeId, self.name))
+
+    def hasTransitNode(self, nodeId):
+        """Return True if the route has a node with the given id otherwise false"""
+        return nodeId in [tn.nodeId for tn in self.iterTransitNodes()]
+        
+    def iterTransitNodes(self):
+        """Return an iterator to the transit nodes"""
+        return iter(self._transitNodes)
+
+    def iterTransitStops(self):
+
+        for trNode in self.iterTransitNodes():
+            if trNode.isStop:
+                yield trNode
+
+    def isFirstNode(self, nodeId):
+        """Return True if the input node id belongs to the first transit node 
+        of the route"""
+        if self.getNumTransitNodes() == 0:
+            raise TPPluseError("Route %s does not have any transit nodes" % self.name)
+        if self._transitNodes[0].nodeId == nodeId:
+            return True
+        else:
+            return False
+
+    def isLastNode(self, nodeId):
+        """Return True if the input node id is the last node in the route"""
+
+        if self.getNumTransitNodes() == 0:
+            raise TPPluseError("Route %s does not have any transit nodes" % self.name)
+        if self._transitNodes[-1].nodeId == nodeId:
+            return True
+        else:
+            return False
+
+    def getNumTransitNodes(self):
+        """Return the number of transit nodes in the route"""
+        return len(self._transitNodes)
+
+    def getNumStops(self):
+        """Return the number of stops the route makes"""
+        return sum([tr.isStop for tr in self.iterTransitNodes()])
+        
---
+++ scripts/importTPPlusTransitRoutes.py
@@ -0,0 +1,160 @@
+__copyright__   = "Copyright 2011 SFCTA"
+__license__     = """
+    This file is part of DTA.
+
+    DTA is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DTA is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with DTA.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import logging
+import sys
+
+from pbCore.utils.itertools2 import pairwise
+from pbCore.dynameq.transitLine import TransitLine
+from pbModels.algorithms.shortestPaths import ShortestPaths
+from pbCore.dynameq.error import TPPlus2DynameqError
+
+def convertHeadway2HHMM(headwayInMin):
+
+    hours = headwayInMin / 60
+    minutes = headwayInMin - hours * 60
+
+    if hours < 10:
+        hours = '0%d' % hours
+    else:
+        hours = '%d' % hours
+    if minutes < 10:
+        minutes = '0%d' % minutes
+    else:
+        minutes = '%d' % minutes
+
+    return "%s%s" % (hours, minutes)
+
+def convertStartTime2HHMMSS(startTimeInMin):
+
+    hours = startTimeInMin / 60
+    minutes = startTimeInMin - hours * 60
+
+    if hours < 10:
+        hours = '0%d' % hours
+    else:
+        hours = '%d' % hours
+    if minutes < 10:
+        minutes = '0%d' % minutes
+    else:
+        minutes = '%d' % minutes
+
+    return "%s:%s:00" % (hours, minutes)
+
+
+
+class TPPlus2Dynameq(object):
+    """Converts TPPlus Network elemements to the equivalent Dynameq ones"""
+
+    @classmethod
+    def convertRoute2(cls, dynameqNet, tpplusRoute):
+
+        DWELL_TIME = 30
+
+        tRoute = tpplusRoute
+
+        dRoute = TransitLine(dynameqNet, tRoute.name, 'label1', '0', 'Generic', '15:30:00', '00:20:00', 10)
+
+        dNodeSequence = []
+        for tNode in tRoute.iterTransitNodes():
+            if not dynameqNet.hasNode(tNode.nodeId):
+                continue
+            dNode = dynameqNet.getNode(tNode.nodeId)
+            dNodeSequence.append(dNode)
+
+        if len(dNodeSequence) == 0:
+            errorMessage = ('Tpplus route %s cannot be converted to Dynameq because '
+                            'none of its nodes is in the Dynameq network' % tRoute.name)
+            logging.error(errorMessage)
+            raise TPPlus2DynameqError(errorMessage)
+                                              
+        if len(dNodeSequence) == 1:
+            errorMessage = ('Tpplus route %s cannot be converted to Dyanmeq because only '
+                                      'one of its nodes is in the Dynameq network' % tRoute.name)
+            logging.error(errorMessage)            
+            raise TPPlus2DynameqError(errorMessage)
+        
+
+    
+    @classmethod
+    def convertRoute(cls, dynameqNet, tpplusRoute, doShortestPath=True):
+        """Convert the input tpplusRoute to an equivalent Dynameq route"""
+        DWELL_TIME = 30
+
+        tRoute = tpplusRoute
+
+        for edge in dynameqNet.iterEdges():
+            edge.cost = edge.getFreeFlowTTInMin()
+            if edge.isConnector():
+                edge.cost = sys.maxint
+
+        
+        dNodeSequence = []
+        for tNode in tRoute.iterTransitNodes():
+            if not dynameqNet.hasNode(tNode.nodeId):
+                continue
+            dNode = dynameqNet.getNode(tNode.nodeId)
+            dNodeSequence.append(dNode)
+
+        if len(dNodeSequence) == 0:
+            errorMessage = ('Tpplus route %s cannot be converted to Dynameq because '
+                            'none of its nodes is in the Dynameq network' % tRoute.name)
+            logging.error(errorMessage)
+            raise TPPlus2DynameqError(errorMessage)
+                                              
+        if len(dNodeSequence) == 1:
+            errorMessage = ('Tpplus route %s cannot be converted to Dyanmeq because only '
+                                      'one of its nodes is in the Dynameq network' % tRoute.name)
+            logging.error(errorMessage)            
+            raise TPPlus2DynameqError(errorMessage)
+
+        dRoute = TransitLine(dynameqNet, tRoute.name, 'label1', '0', 'Generic', '15:30:00', '00:20:00', 10)
+        for dNodeA, dNodeB in pairwise(dNodeSequence):
+            
+            if dynameqNet.hasLink(dNodeA.id, dNodeB.id):
+                dLink = dynameqNet.getLink(dNodeA.id, dNodeB.id)
+                dSegment = dRoute.addSegment(dLink, 0)
+                #print 'added link', dLink.iid
+
+                tNodeB = tRoute.getTransitNode(dLink.nodeBid)
+                if tNodeB.isStop:
+                    dSegment.dwell = DWELL_TIME
+                
+            else:
+                if doShortestPath:
+                    print 'I am running the SP. Root node', dNodeA.id
+                    ShortestPaths.labelCorrecting(dynameqNet, dNodeA)
+                    if dNodeB.label == sys.maxint:
+                        continue
+
+                    pathNodes = ShortestPaths.getShortestPath(dNodeA, dNodeB)
+
+                    for pathNodeA, pathNodeB in pairwise(pathNodes):
+                        dLink = dynameqNet.getLink(pathNodeA.id, pathNodeB.id)
+                        dSegment = dRoute.addSegment(dLink, 0)
+
+                    tNodeB = tRoute.getTransitNode(dLink.nodeBid)
+                    if tNodeB.isStop:
+                        dSegment.dwell = DWELL_TIME
+                else:
+                    pass
+                        
+        
+        dRoute.isPathValid()
+        return dRoute
+                
